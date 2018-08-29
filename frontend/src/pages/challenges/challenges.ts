8dfab87112c42bb6c87371112fcb4ccdb9618dd1
gjeanmart <gregoire.jeanmart@consensys.net>
2018-08-29 20:52:52 +0100

    first commit

---
+++ frontend/src/pages/challenges/challenges.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+ 
+
+@Component({
+  selector: 'page-challenges',
+  templateUrl: 'challenges.html'
+})
+export class ChallengesPage {
+    public category: any;
+  	public obs: Observable<any>;
+    public challenges: any;
+
+  	constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+      this.challenges = {};
+      this.category = "love";
+      this.obs = this.httpClient.get('http://192.168.0.24:8080/api/challenge');
+      this.obs
+      .subscribe(data => {
+        for (var i = 0; i < data.length; i++) {
+            if(!this.challenges[data[i].category]) this.challenges[data[i].category] = [];
+            this.challenges[data[i].category].push(data[i])
+        }
+      })
+
+  	}
+
+    getPictures() {    
+      console.log("get pics")
+    }
+}
